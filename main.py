4cbe3c752cfa80fd89c2e3c585c01fba1f94026f
tasnimalfak <tas172006@gmail.com>
2026-09-13 19:33:38 +0300

    Add more student information

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = "Tasnim Alfakhouri" 
 major = "Data Science and AI" 
 university = "Applied Science University" 
+graduation_year = 2028
 print("Student Profile") 
 print("----------------") 
 print("Name:", name) 
 print("Major:", major) 
-print("University:", university) 
+print("University:", university)
+print("Graduation year", graduation_year)
