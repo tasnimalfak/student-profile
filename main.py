98c43c321afc2121de2e06e4e73fd456ea376fce
tasnimalfak <tas172006@gmail.com>
2026-09-13 19:02:50 +0300

    Add more student information

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Tasnim Alfakhouri" 
+major = "Data Science and AI" 
+university = "Applied Science University" 
+print("Student Profile") 
+print("----------------") 
+print("Name:", name) 
+print("Major:", major) 
+print("University:", university) 
